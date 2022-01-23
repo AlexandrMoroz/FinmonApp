88b1dd5f04c958b082fe092d9f61eda37e04d741
Josier515 <finoduait2@gmail.com>
2022-01-23 13:53:31 +0200

    Add reused function to helper service

--- frontend/src/app/services/helpers.service.ts
+++ frontend/src/app/services/helpers.service.ts
@@ -3,14 +3,20 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { FormlyConfig } from '@ngx-formly/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HelperService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private config: FormlyConfig,
+    private flashMessagesService: FlashMessagesService
+  ) {}
   countryHelper = 'countries';
-  
+
   typesOfBusiness: Array<String> = [
     'юридичні послуги',
     'консалтингові послуги',
@@ -37,7 +43,7 @@ export class HelperService {
     'види діяльності, які передбачають отримання спеціальних дозволів на користування надрами в межах території України, її континентального шельфу та виключної (морської) економічної зони;',
     'державні закупівлі.',
   ];
-  getCountries(){
+  getCountries() {
     if (!localStorage.getItem(this.countryHelper)) {
       return this.http
         .get(`${environment.apiUrl}helper`, {
@@ -73,20 +79,88 @@ export class HelperService {
       return of(JSON.parse(localStorage.getItem(name)));
     }
   }
-  getTypeOfBusiness(){
-    if (!localStorage.getItem("TofB")) {
+  getTypeOfBusiness() {
+    if (!localStorage.getItem('TofB')) {
       return this.http
         .get(`${environment.apiUrl}helper`, {
-          params: new HttpParams().set('name', "typesOfBusiness"),
+          params: new HttpParams().set('name', 'typesOfBusiness'),
         })
         .pipe(
           map((data) => {
-            localStorage.setItem("TofB", JSON.stringify(data['result']));
+            localStorage.setItem('TofB', JSON.stringify(data['result']));
             return data['result'];
           })
         );
     } else {
-      return of(JSON.parse(localStorage.getItem("TofB")));
+      return of(JSON.parse(localStorage.getItem('TofB')));
     }
+  }
+  cleanObject(object) {
+    Object.entries(object).forEach(([k, v]) => {
+      if (v && typeof v === 'object') {
+        this.cleanObject(v);
+      }
+      if (
+        (v && typeof v === 'object' && !Object.keys(v).length) ||
+        v === null ||
+        v === undefined
+      ) {
+        if (Array.isArray(object)) {
+          object.splice(k as any, 1);
+        } else {
+          delete object[k];
+        }
+      }
+    });
+    return object;
+  }
+  isObject(x: any) {
+    return x != null && typeof x === 'object';
+  }
+  getErrorMessage(field) {
+    const fieldForm = field.formControl;
+    for (const error in fieldForm.errors) {
+      if (fieldForm.errors.hasOwnProperty(error)) {
+        let message = this.config.getValidatorMessage(error);
+
+        if (this.isObject(fieldForm.errors[error])) {
+          if (fieldForm.errors[error].errorPath) {
+            return;
+          }
+          if (fieldForm.errors[error].message) {
+            message = fieldForm.errors[error].message;
+          }
+        }
+
+        if (field.validation?.messages?.[error]) {
+          message = field.validation.messages[error];
+        }
+
+        if (field.validators?.[error]?.message) {
+          message = field.validators[error].message;
+        }
+
+        if (field.asyncValidators?.[error]?.message) {
+          message = field.asyncValidators[error].message;
+        }
+        if (typeof message === 'function') {
+          return message(fieldForm.errors[error], field);
+        }
+        return message;
+      }
+    }
+  }
+  ShowError(data) {
+    //console.error(data.splite("</br>"))
+    this.flashMessagesService.show(`Помилка ${data.toString()}`, {
+      cssClass: 'alert-danger',
+      timeout: 5000,
+    });
+  }
+  ShowSuccess(message) {
+    this.flashMessagesService.show(message, {
+      cssClass: 'alert-success',
+      timeout: 5000,
+    });
   }
 }
