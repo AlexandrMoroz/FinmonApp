0f6bbf7a75650b8e169604277be9cd086c505cf5
Josier515 <finoduait2@gmail.com>
2022-02-22 11:05:10 +0200

    add button form get all client in xlsx file

--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -1,68 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
-import { CompanyService } from '../services/company.service';
-import { PersonService } from '../services/person.service';
-
+import { HelperService } from '../services/helpers.service';
+import * as XLSX from 'xlsx';
+import { tap } from 'rxjs/operators';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent {
   isLoading: boolean = false;
-  form = new FormGroup({});
-  model = {};
-  options: FormlyFormOptions = {};
-  fields = [
-    {
-      className: 'col-3 p-3',
-      key: 'Category',
-      type: 'radio',
-      templateOptions: {
-        change: (field: FormlyFieldConfig, event?: any) => {},
-        label: 'Пошук в категорії',
-        options: [
-          {
-            label: 'Фізичні особи',
-            value: 1,
-          },
-          {
-            label: 'Юридичні  особи',
-            value: 2,
-          },
-        ],
-      },
-    },
-    {
-      className: 'col-9 p-3',
-      key: 'SearchText',
-      type: 'input',
-      templateOptions: {
-        label: 'Результат пошуку',
-        change: (field: FormlyFieldConfig, event?: any) => {},
-      },
-    },
-    {
-      className: 'col-6 p-3',
-      key: 'TerroristSearchResult',
-      type: 'textarea',
-      templateOptions: { label: 'Результат пошуку в базі террорістів' },
-    },
-    {
-      className: 'col-6 p-3',
-      key: 'SanctionSearchResult',
-      type: 'textarea',
-      templateOptions: { label: 'Результат пошуку в базі підсанкційних осіб' },
-    },
-  ];
-  constructor(
-    private personService: PersonService,
-    private companyService: CompanyService
-  ) {}
 
-  ngOnInit(): void {}
-  Submit(model){
-    
+  constructor(private helperService: HelperService) {}
+  DownloadAllClient() {
+    this.isLoading = true;
+    this.helperService
+      .getAllClients()
+      .pipe(
+        tap(() => {
+          this.isLoading = false;
+        })
+      )
+      .subscribe((data: any) => {
+        const wb = XLSX.read(data.result, { type: 'base64' });
+        XLSX.writeFile(wb, `Усі клієнти.xlsx`);
+      }, this.helperService.ShowError);
   }
 }
