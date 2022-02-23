e4e2e7f266b8ad9b940c0d9d72d332104193390c
Josier515 <finoduait2@gmail.com>
2022-02-23 17:07:26 +0200

    .

--- backend/test/test.js
+++ backend/test/test.js
@@ -1,13 +1,13 @@
 const { testConfig } = require("../config/index");
 let server = require("../server")(testConfig);
 
-// require("./user")(server);
-// require("./company")(server);
-// require("./person")(server);
-// require("./form")(server);
-// require("./helper")(server);
-// require("./history")(server);
-// require("./questions")(server);
-// require("./sanction")(server);
-// require("./terrorist")(server);
+require("./user")(server);
+require("./company")(server);
+require("./person")(server);
+require("./form")(server);
+require("./helper")(server);
+require("./history")(server);
+require("./questions")(server);
+// // require("./sanction")(server);
+// // require("./terrorist")(server);
 require("./clienlist")(server);
