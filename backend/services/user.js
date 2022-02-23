c40728efa51b890c5a189062b2d8bc3351fe38a2
Josier515 <finoduait2@gmail.com>
2022-02-23 15:53:32 +0200

    add delelte func and password hashing

--- backend/services/user.js
+++ backend/services/user.js
@@ -1,5 +1,5 @@
 const { serializeUser } = require("../utils/auth");
-
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 class UserService {
   constructor() {
@@ -21,14 +21,22 @@ class UserService {
       surname: body.surname ? body.surname : "",
       cashboxAdress: body.cashboxAdress ? body.cashboxAdress : "",
       email: body.email ? body.email : "",
-      role: "user",
+      role: body.role,
       username: body.username,
       password: password,
     }).save();
     return serializeUser(createdUser);
   }
-  async edit(body) {
-    let editedUser = await User.findOneAndUpdate({ _id: body.id }, { ...body });
+  async edit(user) {
+    if (user.password) user.password = await bcrypt.hash(user.password, 12);
+
+    let editedUser = await User.findOneAndUpdate(
+      { _id: user.id },
+      {
+        ...user,
+      },
+      { new: true }
+    );
     return serializeUser(editedUser);
   }
   async getAll() {
@@ -37,7 +45,9 @@ class UserService {
       return serializeUser(item);
     });
   }
-  
-    
+
+  async deleteAll() {
+    await User.deleteMany({});
+  }
 }
 module.exports = new UserService();
