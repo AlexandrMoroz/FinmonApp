1a353c110d9bca5ddfa3a90956eeabf64689d53a
Josier515 <finoduait2@gmail.com>
2022-02-23 15:52:52 +0200

    refactor

--- backend/services/sanction.js
+++ backend/services/sanction.js
@@ -3,12 +3,13 @@ const companySanction = require("../models/companySanction");
 const axios = require("axios");
 
 class SanctionService {
-  constructor() {}
+  constructor() {
+    this.personsSanctionUrl = "https://sanctions-t.rnbo.gov.ua/api/fizosoba/";
+    this.companySanctionUrl = "https://sanctions-t.rnbo.gov.ua/api/jurosoba/";
+  }
   async updatePersonFromSite() {
     try {
-      let res = await axios.get(
-        "https://sanctions-t.rnbo.gov.ua/api/fizosoba/"
-      );
+      let res = await axios.get(this.personsSanctionUrl);
       console.log("Sanction person update start");
 
       if (res.status == 200) {
@@ -25,9 +26,7 @@ class SanctionService {
   }
   async updateCompanyFromSite() {
     try {
-      let res = await axios.get(
-        "https://sanctions-t.rnbo.gov.ua/api/jurosoba/"
-      );
+      let res = await axios.get(this.companySanctionUrl);
       if (res.status == 200) {
         console.log("Sanction Company update start");
         await companySanction.deleteMany({});
